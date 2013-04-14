efd53ba4567874f38b2037a0919c9275703010c9
Dominik Schürmann <dominik@dominikschuermann.de>
2013-04-14 21:45:31 +0200

    Add requestDisallowInterceptTouchEvent(true) to onTouchEvent to get HoloColorPicker working in ScrollView

--- src/com/larswerkman/colorpicker/SVBar.java
+++ src/com/larswerkman/colorpicker/SVBar.java
@@ -257,6 +257,8 @@ public class SVBar extends View {
 
 	@Override
 	public boolean onTouchEvent(MotionEvent event) {
+		getParent().requestDisallowInterceptTouchEvent(true);
+
 		// Convert coordinates to our internal coordinate system
 		float x = event.getX();
 		float y = event.getY();
