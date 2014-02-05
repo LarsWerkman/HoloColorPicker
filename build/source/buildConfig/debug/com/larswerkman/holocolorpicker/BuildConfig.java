42193b376c0adf4e081b04f93c04fa99e0feeda5
Lars Werkman <werkman.lars@gmail.com>
2014-02-05 17:18:41 +0100

    gitignore update

---
+++ build/source/buildConfig/debug/com/larswerkman/holocolorpicker/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.larswerkman.holocolorpicker;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String PACKAGE_NAME = "com.larswerkman.holocolorpicker";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 12;
+}
---
+++ build/source/buildConfig/release/com/larswerkman/holocolorpicker/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.larswerkman.holocolorpicker;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String PACKAGE_NAME = "com.larswerkman.holocolorpicker";
+  public static final String BUILD_TYPE = "release";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 12;
+}
